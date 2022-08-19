de01183e2289e39c9a065b1cbb218dc9dc087996
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-08-19 20:21:31 +0300

    create new Components

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a 
+                    class="navbar-brand" 
+                    href="#"
+                    >
+                        Navbar
+                        </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <Link 
+                                class="nav-link" 
+                                to="/home"
+                                >
+                                    Home
+                                    </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link 
+                                class="nav-link" 
+                                to="/about"
+                                >
+                                    About
+                                    </Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link 
+                                class="nav-link" 
+                                to="/todolist"
+                                >
+                                    To do List
+                                    </Link>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Header;
