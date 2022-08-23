e474e1a4dd90eccbfe48acc9a814cfcd9054e9e8
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-08-23 20:30:35 +0300

    logo added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,47 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../Images/logo.svg'
 
 const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <a 
-                    className="navbar-brand" 
-                    href="#"
+                    <Link
+                        className="navbar-brand"
+                        to="/home"
                     >
-                        Navbar
-                        </a>
+                        <img
+                            src={logo}
+                            alt=''
+                            width="50px"
+                        />
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link 
-                                className="nav-link" 
-                                to="/home"
+                                <Link
+                                    className="nav-link"
+                                    to="/home"
                                 >
                                     Home
-                                    </Link>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link 
-                                className="nav-link" 
-                                to="/about"
+                                <Link
+                                    className="nav-link"
+                                    to="/about"
                                 >
                                     About
-                                    </Link>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link 
-                                className="nav-link" 
-                                to="/todolist"
+                                <Link
+                                    className="nav-link"
+                                    to="/todolist"
                                 >
                                     To do List
-                                    </Link>
+                                </Link>
                             </li>
-                            
+
                         </ul>
                     </div>
                 </div>
