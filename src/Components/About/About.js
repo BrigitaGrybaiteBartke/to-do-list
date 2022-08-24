b08a8db96c11009c9f85225deb94c4c9def68916
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-08-24 20:24:45 +0300

    modified about component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 // import '../About/About.css'
+import call from '../../Images/Icons/call.svg'
+import mail from '../../Images/Icons/mail.svg'
+import map from '../../Images/Icons/map.svg'
 
 
 const About = () => {
     return (
         <>
             <div className='container'>
-                <div className=''>
-                    <h1 className=''>About</h1>
+                <div className='m-5 text-center'>
+                    <h4>About Us</h4>
+                </div>
+                <div className='d-flex justify-content-evenly m-5'>
+                    <div>
+                        <div className='pb-2 text-center'>
+                            <img
+                                className='text-center'
+                                src={call}
+                                alt='appLogo'
+                                width="30px"
+                            />
+                        </div>
+                        <div>
+                            <span>+370 (333) 33 333</span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='pb-2 text-center'>
+                            <img
+                                src={mail}
+                                alt='appLogo'
+                                width="30px"
+                            />
+                        </div>
+                        <div>
+                            <span>
+                                <a href="mailto:email@example.com">email@example.com</a>
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='pb-2 text-center'>
+                            <img
+                                src={map}
+                                alt='appLogo'
+                                width="30px"
+                            />
+                        </div>
+                        <span>
+                            Street 33, LT-33333, Lithuania
+                        </span>
+                        <div>
+
+                        </div>
+                    </div>
+
                 </div>
                 <ul className="nav nav-tabs justify-content-center">
                     <li className="nav-item">
