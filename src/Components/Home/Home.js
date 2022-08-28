638ce9fc5027e80c74a6e5e5b83a0ad977cdd770
BrigitaGrybaiteBartke <brigita.grybaite@gmail.com>
2022-08-28 20:40:46 +0300

    modify home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import sofa from '../../Images/Icons/sofa.svg'
 
 const Home = () => {
     return (
         <>
-        Home
+            <div className='container'>
+                <div className='d-flex flex-column align-items-center justify-content-center mt-4 mb-4'>
+                    <div className='mb-5'>
+                        <h3>Welcome to my Page!</h3>
+                    </div>
+                    <div className='row'>
+                        <img
+                            src={sofa}
+                            alt='homePageImage'
+                            style={{ width: '500px' }}
+                        />
+                    </div>
+                </div>
+            </div>
         </>
-    );
-};
+    )
+}
 
 export default Home;
